4c7822cc342f2eb53066ecb7490aefda9b888808
SindiswaN <166315391+SindiswaN@users.noreply.github.com>
2024-04-11 20:46:31 +0200

    Update Program.cs

--- part1/Program.cs
+++ part1/Program.cs
@@ -2,7 +2,7 @@
 
 public class Recipe
 {
- 
+ //properties of a recipe
     public string Name { get; set; }
     public Ingredient[] Ingredients { get; set; }
     public string[] Steps { get; set; }
